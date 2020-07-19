4cb29e61fd59f887de04db7cbf439bbeea2c99ee
Sherif <sherift1552@gmail.com>
2020-07-19 16:00:25 +0000

    add articles resource routes

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ Route::get('/', 'HomeController@index')->name('home');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('articles', 'ArticleController');
