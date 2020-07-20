27176deb9b5dc7126d67f9ac975e2fb8b69124ce
Sherif <sherift1552@gmail.com>
2020-07-20 10:00:41 +0000

    add policy to authorize request on article resource

---
+++ app/Policies/ArticlePolicy.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Policies;
+
+use App\Article;
+use App\Author;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class ArticlePolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Author  $author
+     * @param  \App\Article  $article
+     * @return mixed
+     */
+    public function update(Author $author, Article $article)
+    {
+        return $article->isWrittenBy($author);
+    }
+}
