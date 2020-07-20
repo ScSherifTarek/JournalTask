310503c6b5cb22ac0ab2a409d79e8080b925cf64
Sherif <sherift1552@gmail.com>
2020-07-20 12:18:58 +0000

    add utility methods to the author to check if this author is an admin or to make him an admin

--- app/Author.php
+++ app/Author.php
@@ -15,4 +15,25 @@ class Author extends User
     {
         return $this->hasMany(Article::class, 'author_id', 'id');
     }
+
+    /**
+     * Check if the current author is an admin
+     * 
+     * @return boolean
+     */
+    public function isAdmin(): bool
+    {
+    	return $this->role == 'admin';
+    }
+
+    /**
+     * Make this author an admin
+     * 
+     * @return boolean
+     */
+    public function makeAdmin(): bool
+    {
+    	$this->role == 'admin';
+    	return $this->save();
+    }
 }
