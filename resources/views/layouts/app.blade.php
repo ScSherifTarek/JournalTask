04171ad06e28bc44932796784a2a50b1f3bd1508
Sherif <sherift1552@gmail.com>
2020-07-20 14:23:51 +0000

    add articles tab to the navigation bar so admins and authors can access it easily

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,6 +26,11 @@
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
+                @auth
+                <a class="navbar-brand" href="{{ route('articles.index') }}">
+                    @lang('common.articles')
+                </a>
+                @endauth
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
