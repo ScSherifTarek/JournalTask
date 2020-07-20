711c6a0b4af3024a3c3c208d9d58186a3ebc4da9
Sherif <sherift1552@gmail.com>
2020-07-20 13:38:44 +0000

    update the author name and counter to say that some of them may not be approved yet

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -2,5 +2,5 @@
 
 return [
 	'no_articles' => 'No articles created yet!',
-	'written_by_with_count' => 'Written by :author who wrote :count articles',
+	'written_by_with_count' => 'Written by :author who wrote :count articles but some of them may be not approved yet 😉',
 ];
